49d4dbee61faca36043221ba8e9457701f545251
Edwin Chen <edwinchen85@gmail.com>
2018-05-23 09:52:00 +0800

    Create required pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { MyApp } from "./app.component";
+import { EditRecipePage } from "../pages/edit-recipe/edit-recipe";
+import { ShoppingListPage } from "../pages/shopping-list/shopping-list";
+import { RecipesPage } from "../pages/recipes/recipes";
+import { RecipePage } from "../pages/recipe/recipe";
+import { TabsPage } from "../pages/tabs/tabs";
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    TabsPage
   ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    EditRecipePage,
+    ShoppingListPage,
+    RecipesPage,
+    RecipePage,
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
